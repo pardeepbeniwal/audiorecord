b29eb556b712be3839dcc554c07828229f4dc6dc
pardeepbeniwal84 <pardeepbeniwal84@gmail.com>
2017-07-11 13:55:10 +0530

    saving vedio file

--- api/v1/controllers/users.js
+++ api/v1/controllers/users.js
@@ -6,7 +6,7 @@ module.exports = {
      },      
 	 uploads: function(req,res){
 			 var buf = new Buffer(req.body.blob, 'base64'); // decode
-			  fs.writeFile("public/uploads/test.wav", buf, function(err) {
+			  fs.writeFile("public/uploads/test.mp4", buf, function(err) {
 				if(err) {
 				  console.log("err", err);
 				} else {
